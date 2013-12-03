3c8bd8b24f7b540d6f93d461dfcf0108e3362d40
rightgo09 <skyarrow09@gmail.com>
2013-12-03 23:18:55 +0900

    rails new sample_app --skip-test-unit --skip-bundle
    refs. http://railstutorial.jp/chapters/static-pages?version=4.0#top

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '8f58a54b78fc347f22946b9e9e6655f5085c5efd40ea69bdc8538142270d313e3777a7803b9ab88b25df43b596a8f5df0033708d5ea118582e54fc3e0b0ec396'
