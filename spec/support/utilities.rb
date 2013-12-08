c6ac896350ae0b139afb58f083f8d043a362c05c
rightgo09 <skyarrow09@gmail.com>
2013-12-08 15:05:03 +0900

    Edit user link

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,15 @@
 include ApplicationHelper
 
-def valid_signin(user)
-  fill_in "Email",    with: user.email
-  fill_in "Password", with: user.password
-  click_button "Sign in"
+def valid_signin(user, options = {})
+  if options[:no_capybara]
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
 end
 
 RSpec::Matchers.define :have_error_message do |message|
