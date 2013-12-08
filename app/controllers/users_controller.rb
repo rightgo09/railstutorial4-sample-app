1f52bbe2f5eee617f4998b864f7e93f54efbb83e
rightgo09 <skyarrow09@gmail.com>
2013-12-08 18:37:38 +0900

    Remove coded depulicated by before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,11 +21,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       redirect_to @user, flash: { success: "Profile updated" }
     else
