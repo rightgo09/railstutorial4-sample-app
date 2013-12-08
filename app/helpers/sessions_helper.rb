a4e117eaffaae72d17e64a6a85103d97fce22ab8
rightgo09 <skyarrow09@gmail.com>
2013-12-08 13:53:49 +0900

    Sign out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,4 +18,9 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(remember_token: User.encrypt(cookies[:remember_token]))
   end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
 end
