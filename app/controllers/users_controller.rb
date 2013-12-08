8f2c53545af8f3a32c65a4ff5513a8a7113fb83e
rightgo09 <skyarrow09@gmail.com>
2013-12-08 18:24:43 +0900

    before_action :signed_in_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -34,5 +36,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def signed_in_user
+    redirect_to signin_url, flash: { warning: "Please sign in." } unless signed_in?
   end
 end
